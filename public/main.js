4fc62f2739127eeda67ae114ea4dc41aa4040b35
Kevin Yan <k3vinyan@gmail.com>
2016-11-03 02:27:37 -0700

    card is deleted from hand when chosen

--- public/main.js
+++ public/main.js
@@ -3,12 +3,44 @@
   var cardGame = angular.module('cardGame', []);
 
   cardGame.controller('gameCtrl', function($scope) {
+    var playerOneScore = 0;
+    var playerTwoScore = 0;
+
+
     var displayCard = {};
-    $scope.cards = cards;
+    $scope.playerOneCards = cards;
+    $scope.playerTwoCards = cards;
     $scope.pickCard = function(card) {
-      console.log(card)
       $scope.displayCard = card;
+      compareCard(card, cards[0]);
+
     };
+
+    var removeCard = function(cards, cardPick) {
+      for(var i = 0; i < cards.length; i++) {
+        if(cards[i] === cardPick) {
+          console.log(cards[i]);
+          cards.splice(i, 1);
+        }
+      }
+    };
+
+    var compareCard = function(playerOneCard, playerTwoCard) {
+      ;
+        if(playerOneCard.value > playerTwoCard.value) {
+            removeCard($scope.playerOneCards, playerOneCard);
+            playerOneScore++;
+              console.log("cat")
+        } else if(playerOneCard.value < playerTwoCard.value) {
+            removeCard($scope.playerTwoCards);
+            playerTwoScore++;
+              console.log("dog")
+        } else {
+            console.log("monkey")
+        }
+    };
+
   });
+
 
 })();
