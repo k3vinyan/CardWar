64b4e013438d36f620e1a69d243f3faff536677c
Kevin Yan <k3vinyan@gmail.com>
2016-11-04 06:35:22 -0700

    new rules add to cards.js

--- public/cards.js
+++ public/cards.js
@@ -1,145 +1,11 @@
-var player1 = [
-    { name: "Musician",
-      value: 0,
-      desc: "This round is nullifed and put on hold",
-      priority: 1000,
-      power: {
-        type: "carry",
-        cast: function(point){
-          var point = 1;
-        }
-      }
-    },
-    {
-      name: "Princess",
-      value: 1,
-      desc: "If your opponent plays the prince, you automatically win the game",
-      priority: 6,
-      power: {
-        type: "win",
-        cast: function(playerCard, opponentCard){
-          if(opponentCard.name === "Prince") {
-            console.log("Game Over");
-          }
-        }
-      }
-    },
-    {
-      name: "Spy",
-      value: 2,
-      desc: "Next round, your opponent reveals his card before you choose yours",
-      priority: 5,
-      power: {
-        type: "carry",
-        cast: function(){
-          console.log("I am a spy")
-        }
-      }
-    },
-    {
-      name: "Assassin",
-      value: 3,
-      desc: "The lowest strength wins",
-      priority: 4,
-      power: {
-        type: "instance",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          if(playerCard.value < opponentCard.value) {
-            playerScore++;
-          } else {
-            opponentScore++;
-          }
-        }
-      }
-    },
-    {
-      name: "Ambassador",
-      value: 4,
-      desc: "If you win with this card, it counts as 2 victories",
-      priority: 3,
-      power: {
-        type: "instance",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          if(playerCard > opponentCard) {
-            playerScore += 2;
-          } else {
-            opponentScore++;
-          }
-        }
-      }
-    },
-    {
-      name: "Wizard",
-      value: 5,
-      desc: "Nullifies the special Power of your opponent card",
-      priority: 10,
-      power: {
-        type: "instance",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          if(playerCard.value > opponentCard.value){
-            playerScore++;
-          } else {
-            opponentScore++
-          }
-        }
-      }
-    },
-    {
-      name: "General",
-      value: 6,
-      desc: "The Card in your next round get +2 strength",
-      priority: 1,
-      power: {
-        type: "carry",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          console.log("Nothing yet");
-        }
-      }
-    },
-    {
-      name: "General",
-      value: 6,
-      desc: "The Card in your next round get +2 strength",
-      priority: 1,
-      power: {
-        type: "carry",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          console.log("Nothing yet");
-        }
-      }
-    },
-    {
-      name: "General",
-      value: 6,
-      desc: "The Card in your next round get +2 strength",
-      priority: 1,
-      power: {
-        type: "carry",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          console.log("Nothing yet");
-        }
-      }
-    },
-    {
-      name: "Prince",
-      value: 7,
-      desc: "You win the round",
-      priority: 0,
-      power: {
-        type: "instance",
-        cast: function(playerCard, opponentCard, playerScore, opponentScore){
-          if(playerCard.value > opponentCard){
-            playerScore++;
-          } else {
-            opponentScore++;
-          }
-        }
-      }
-    }
-  ]
+var playerOne = {};
+var playerTwo ={};
+playerOne.score = 0;
+playerTwo.score = 0;
 
-  var player2 = [
+playerOne.cards = [
       { name: "Musician",
+        player: "player1",
         value: 0,
         desc: "This round is nullifed and put on hold",
         priority: 1000,
@@ -152,11 +18,12 @@ var player1 = [
       },
       {
         name: "Princess",
+        player: "player1",
         value: 1,
         desc: "If your opponent plays the prince, you automatically win the game",
         priority: 6,
         power: {
-          type: "win",
+          type: "instance win",
           cast: function(playerCard, opponentCard){
             if(opponentCard.name === "Prince") {
               console.log("Game Over");
@@ -166,6 +33,7 @@ var player1 = [
       },
       {
         name: "Spy",
+        player: "player1",
         value: 2,
         desc: "Next round, your opponent reveals his card before you choose yours",
         priority: 5,
@@ -178,6 +46,7 @@ var player1 = [
       },
       {
         name: "Assassin",
+        player: "player1",
         value: 3,
         desc: "The lowest strength wins",
         priority: 4,
@@ -194,6 +63,7 @@ var player1 = [
       },
       {
         name: "Ambassador",
+        player: "player1",
         value: 4,
         desc: "If you win with this card, it counts as 2 victories",
         priority: 3,
@@ -210,6 +80,7 @@ var player1 = [
       },
       {
         name: "Wizard",
+        player: "player1",
         value: 5,
         desc: "Nullifies the special Power of your opponent card",
         priority: 10,
@@ -226,30 +97,7 @@ var player1 = [
       },
       {
         name: "General",
-        value: 6,
-        desc: "The Card in your next round get +2 strength",
-        priority: 1,
-        power: {
-          type: "carry",
-          cast: function(playerCard, opponentCard, playerScore, opponentScore){
-            console.log("Nothing yet");
-          }
-        }
-      },
-      {
-        name: "General",
-        value: 6,
-        desc: "The Card in your next round get +2 strength",
-        priority: 1,
-        power: {
-          type: "carry",
-          cast: function(playerCard, opponentCard, playerScore, opponentScore){
-            console.log("Nothing yet");
-          }
-        }
-      },
-      {
-        name: "General",
+        player: "player1",
         value: 6,
         desc: "The Card in your next round get +2 strength",
         priority: 1,
@@ -262,6 +110,133 @@ var player1 = [
       },
       {
         name: "Prince",
+        player: "player1",
+        value: 7,
+        desc: "You win the round",
+        priority: 0,
+        power: {
+          type: "instance",
+          cast: function(playerCard, opponentCard, playerScore, opponentScore){
+            if(playerCard.value > opponentCard){
+              playerScore++;
+            } else {
+              opponentScore++;
+            }
+          }
+        }
+      }
+    ]
+
+
+
+playerTwo.cards = [
+      { name: "Musician",
+        player: "player2",
+        value: 0,
+        desc: "This round is nullifed and put on hold",
+        priority: 1000,
+        power: {
+          type: "carry",
+          cast: function(point){
+            var point = 1;
+          }
+        }
+      },
+      {
+        name: "Princess",
+        player: "player2",
+        value: 1,
+        desc: "If your opponent plays the prince, you automatically win the game",
+        priority: 6,
+        power: {
+          type: "win",
+          cast: function(playerCard, opponentCard){
+            if(opponentCard.name === "Prince") {
+              console.log("Game Over");
+            }
+          }
+        }
+      },
+      {
+        name: "Spy",
+        player: "player2",
+        value: 2,
+        desc: "Next round, your opponent reveals his card before you choose yours",
+        priority: 5,
+        power: {
+          type: "carry",
+          cast: function(){
+            console.log("I am a spy")
+          }
+        }
+      },
+      {
+        name: "Assassin",
+        player: "player2",
+        value: 3,
+        desc: "The lowest strength wins",
+        priority: 4,
+        power: {
+          type: "instance",
+          cast: function(playerCard, opponentCard, playerScore, opponentScore){
+            if(playerCard.value < opponentCard.value) {
+              playerScore++;
+            } else {
+              opponentScore++;
+            }
+          }
+        }
+      },
+      {
+        name: "Ambassador",
+        player: "player2",
+        value: 4,
+        desc: "If you win with this card, it counts as 2 victories",
+        priority: 3,
+        power: {
+          type: "instance",
+          cast: function(playerCard, opponentCard, playerScore, opponentScore){
+            if(playerCard > opponentCard) {
+              playerScore += 2;
+            } else {
+              opponentScore++;
+            }
+          }
+        }
+      },
+      {
+        name: "Wizard",
+        player: "player2",
+        value: 5,
+        desc: "Nullifies the special Power of your opponent card",
+        priority: 10,
+        power: {
+          type: "instance",
+          cast: function(playerCard, opponentCard, playerScore, opponentScore){
+            if(playerCard.value > opponentCard.value){
+              playerScore++;
+            } else {
+              opponentScore++
+            }
+          }
+        }
+      },
+      {
+        name: "General",
+        player: "player2",
+        value: 6,
+        desc: "The Card in your next round get +2 strength",
+        priority: 1,
+        power: {
+          type: "carry",
+          cast: function(playerCard, opponentCard, playerScore, opponentScore){
+            console.log("Nothing yet");
+          }
+        }
+      },
+      {
+        name: "Prince",
+        player: "player2",
         value: 7,
         desc: "You win the round",
         priority: 0,
--- public/main.js
+++ public/main.js
@@ -2,40 +2,139 @@
 
   var cardGame = angular.module('cardGame', []);
 
+
   cardGame.controller('gameCtrl', function($scope) {
-    $scope.playerOneScore = 0;
-    $scope.playerTwoScore = 0;
-    $scope.playerOneCards = player1;
-    $scope.playerTwoCards = player2;
+    $scope.playerOneScore = playerOne.score;
+    $scope.playerTwoScore = playerTwo.score;
+    $scope.playerOne = playerOne;
+    $scope.playerTwo = playerTwo;
 
-    $scope.startRound = function(card, playerOneCards, playerTwoCards, compareCard, deleteCards){
-      var playerOneCard = card;
-      var playerTwoCard = randomCard(playerTwoCards);
-      $scope.playerOneCard = card
-      $scope.playerTwoCard = playerTwoCard;
-      compareCard(playerOneCard, playerTwoCard, playerOneCards, playerTwoCards, deleteCards);
-    }
-
-    $scope.compareCard = function(card1, card2, player1cards, player2cards, deleteCards){
-        if(card1.value > card2.value){
-          $scope.playerOneScore++;
-        } else if(card1.value < card2.value) {
-           $scope.playerTwoScore++;
-        } else {
-          console.log("tie");
-        }
-        deleteCards(card1, card2, player1cards, player2cards);
+    var carryRound = {
+      points: 0,
+      players: {
+        player1: { value: 0 },
+        player2: { value: 0 }
+      },
+      powers: []
     };
 
-    $scope.deleteCards = function(card1, card2, player1cards, player2cards){
-      for(var i = 0; i < player1cards.length; i++){
-        if(card1 === player1cards[i]){
-          player1cards.splice(i, 1);
+    var assassinPower = function(cardPick, player){
+
+    };
+
+    $scope.startRound = function(cardPick, playerOne, playerTwo, compareCard, deleteCards){
+      var playerOneCardPick = cardPick;
+      var playerTwoCardPick = randomCard(playerTwo.cards);
+      $scope.playerOneCard = playerOneCardPick;
+      $scope.playerTwoCard = playerTwoCardPick;
+      //check for carry Power and excute
+      if(carryRound.powers.length > 0){
+        for(var i = 0; i < carryRound.powers.length; i++) {
+            carryRound.powers[i]();
+        }
+      };
+
+      if(carryRound.players.player1.value > 0){
+        playerOneCardPick.value += carryRound.players.player1.value;
+        carryRound.players.player1.value = 0;
+      } else if(carryRound.players.player2.value > 0){
+        playerTwoCardPick.value += carryRound.players.player2.value;
+        carryRound.players.player1.value = 0;
+      }
+
+      compareCard(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo, deleteCards);
+    }
+
+    $scope.compareCard = function(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo, deleteCards){
+
+      //musician card
+      if(playerOneCardPick.name === "Musician" || playerTwoCardPick.name === "Musician") {
+        carryRound.points++;
+        deleteCards(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo);
+        return
+      }
+
+      //general card
+      if(playerOneCardPick.name === "General"){
+        carryRound.players.player1.value++;
+      } else if(playerTwoCardPick.name === "General"){
+        carryRound.players.player2.value++;
+      }
+
+
+      //comparing Wizard
+      if(playerOneCardPick.name === "Wizard" || playerTwoCardPick.name === "Wizard") {
+        if(playerOneCardPick.value > playerTwoCardPick.value){
+          playerOne.score = carryRound.points + 1;
+          carryRound.points = 0;
+        } else if(playerOneCardPick.value < playerTwoCardPick.value){
+          playerTwo.score = carryRound.points + 1;
+          carryRound.points = 0;
+        } else if(playerOneCardPick.value === playerTwoCardPick.value){
+          console.log("tie");
+          carryRound.point++
         }
       }
-      for(var i = 0; i < player2cards.length; i++){
-        if(card2 === player2cards[i]){
-          player2cards.splice(i, 1);
+
+      //comparing princess card
+      if(playerOneCardPick.name === "princess" && playerTwoCardPick.name === "prince"){
+        console.log("player 1 win");
+      } else if (playerOneCardPick.name === "prince" && playerTwoCardPick.name === "princess"){
+        console.log("player 2 win");
+      }
+
+      //comparing Ambassador
+      if(playerOneCardPick.name === "Ambassador" && playerOneCardPick.value > playerTwoCardPick.value){
+        playerOne.score = carryRound.points + 1;
+        console.log('hi')
+        carryRound.points = 0;
+      } else if(playerTwoCardPick.name === "Ambassador" && playerTwoCardPick.value > playerOneCardPick.value){
+        playerTwo.score = carryRound.points + 1;
+        carryRound.points = 0;
+        console.log('hello')
+      }
+
+      //lowest strength win
+      if(playerOneCardPick.name === "Assassin" || playerTwoCardPick.name === "Assassin"){
+        if(playerOneCardPick.value > playerTwoCardPick.value) {
+          playerTwo.score = carryRound.points + 1;
+          carryRound.points = 0;
+          console.log("hit")
+          deleteCards(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo);
+          return
+        } else if(playerOneCardPick.value < playerTwoCardPick.value){
+          playerOne.score = carryRound.points + 1;
+          carryRound.points = 0;
+          console.log("miss")
+          deleteCards(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo);
+          return
+        }
+      }
+
+      //compare value
+      if(playerOneCardPick.value > playerTwoCardPick.value){
+        playerOne.score = carryRound.points + 1;
+        carryRound.points = 0;
+      } else if(playerOneCardPick.value < playerTwoCardPick.value){
+        playerTwo.score = carryRound.points + 1;
+        carryRound.points = 0;
+      } else if(playerOneCardPick.value === playerTwoCardPick.value){
+        console.log("tie");
+        carryRound.point++
+      }
+
+      deleteCards(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo);
+    };
+
+    $scope.deleteCards = function(playerOneCardPick, playerTwoCardPick, playerOne, playerTwo){
+      for(var i = 0; i < playerOne.cards.length; i++){
+        if(playerOneCardPick === playerOne.cards[i]){
+          playerOne.cards.splice(i, 1);
+        }
+      }
+      for(var i = 0; i < playerTwo.cards.length; i++){
+        if(playerTwoCardPick === playerTwo.cards[i]){
+          playerTwo.cards.splice(i, 1);
         }
       }
     }
@@ -44,49 +143,157 @@
       return deck[Math.floor((Math.random()*deck.length))]
     }
 
+    let gameOver = function(player){
+      if(player === "player1"){
+        alert("player1 win");
+      } else {
+        alert("player2 win");
+      }
+    };
     $scope.reset = function() {
       $scope.playerOneScore = 0;
       $scope.playerTwoScore = 0;
       $scope.playerOneCard = "";
       $scope.playerTwoCard = "";
-
-      this.playerOneCards = this.playerTwoCards = [
-          {
-            name: "one",
-            value: 1
+      console.log("reset....");
+      this.playerOne = this.playerTwo = [
+          { name: "Musician",
+            value: 0,
+            desc: "This round is nullifed and put on hold",
+            priority: 1000,
+            power: {
+              type: "carry",
+              cast: function(point){
+                var point = 1;
+              }
+            }
           },
           {
-            name: "two",
-            value: 2
+            name: "Princess",
+            value: 1,
+            desc: "If your opponent plays the prince, you automatically win the game",
+            priority: 6,
+            power: {
+              type: "win",
+              cast: function(playerCard, opponentCard){
+                if(opponentCard.name === "Prince") {
+                  console.log("Game Over");
+                }
+              }
+            }
           },
           {
-            name: "three",
-            value: 3
+            name: "Spy",
+            value: 2,
+            desc: "Next round, your opponent reveals his card before you choose yours",
+            priority: 5,
+            power: {
+              type: "carry",
+              cast: function(){
+                console.log("I am a spy")
+              }
+            }
           },
           {
-            name: "four",
-            value: 4
+            name: "Assassin",
+            value: 3,
+            desc: "The lowest strength wins",
+            priority: 4,
+            power: {
+              type: "instance",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                if(playerCard.value < opponentCard.value) {
+                  playerScore++;
+                } else {
+                  opponentScore++;
+                }
+              }
+            }
           },
           {
-            name: "five",
-            value: 5
+            name: "Ambassador",
+            value: 4,
+            desc: "If you win with this card, it counts as 2 victories",
+            priority: 3,
+            power: {
+              type: "instance",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                if(playerCard > opponentCard) {
+                  playerScore += 2;
+                } else {
+                  opponentScore++;
+                }
+              }
+            }
           },
           {
-            name: "six",
-            value: 6
+            name: "Wizard",
+            value: 5,
+            desc: "Nullifies the special Power of your opponent card",
+            priority: 10,
+            power: {
+              type: "instance",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                if(playerCard.value > opponentCard.value){
+                  playerScore++;
+                } else {
+                  opponentScore++
+                }
+              }
+            }
           },
           {
-            name: "seven",
-            value: 7
+            name: "General",
+            value: 6,
+            desc: "The Card in your next round get +2 strength",
+            priority: 1,
+            power: {
+              type: "carry",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                console.log("Nothing yet");
+              }
+            }
           },
           {
-            name: "nine",
-            value: 8
+            name: "General",
+            value: 6,
+            desc: "The Card in your next round get +2 strength",
+            priority: 1,
+            power: {
+              type: "carry",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                console.log("Nothing yet");
+              }
+            }
           },
           {
-            name: "nine",
-            value: 9
+            name: "General",
+            value: 6,
+            desc: "The Card in your next round get +2 strength",
+            priority: 1,
+            power: {
+              type: "carry",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                console.log("Nothing yet");
+              }
+            }
           },
+          {
+            name: "Prince",
+            value: 7,
+            desc: "You win the round",
+            priority: 0,
+            power: {
+              type: "instance",
+              cast: function(playerCard, opponentCard, playerScore, opponentScore){
+                if(playerCard.value > opponentCard){
+                  playerScore++;
+                } else {
+                  opponentScore++;
+                }
+              }
+            }
+          }
         ]
 
 
