4277edb2b744a739aa6efccd6ae5bb16e4bc9cbe
Kevin Yan <k3vinyan@gmail.com>
2019-05-06 01:58:56 -0700

    update port env in app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', function(req, res) {
 });
 
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, function(){
-  console.log("server is running on port 3000");
+app.listen(port, function(){
+  console.log("server is running on port: ", port);
 });
